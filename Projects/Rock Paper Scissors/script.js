239ca37ead5dc88226e8add88671eb040243fae0
Sentriarc <tran.christopherd1@gmail.com>
2024-07-22 19:16:07 -0500

    added outline for js file

--- Projects/Rock Paper Scissors/script.js
+++ Projects/Rock Paper Scissors/script.js
@@ -3,3 +3,26 @@ const rock = 1;
 const paper = 2;
 const scissors = 3;
 
+// Declaring values for human & computer scores
+let humanScore = 0;
+let computerScore = 0;
+
+// Linking rock button
+document.querySelector();
+
+// Function for when rock button is clicked
+
+
+// Linking paper button
+document.querySelector();
+
+// Function for when paper button is clicked
+
+
+// Linking scissors button
+document.querySelector();
+
+// Function for when scissors button is clicked
+
+
+// If-statement: After user input (from buttons), computer will randomly select numbers from 1-3 (rock, paper, or scissors)
