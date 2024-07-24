94fa7e1debbee569868383524bf7f206147a9c0f
BelowNeutral <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-24 15:37:55 -0500

    added h1 and buttons to html file

--- Projects/Etch-a-Sketch/script.js
+++ Projects/Etch-a-Sketch/script.js
@@ -1,3 +1,13 @@
+// Function that generates a random color
+function generateRandomColor() {
+    let rgbValue = 0xFFFFFF;
+    let randomNumber = Math.random() * rgbValue;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randomColor = randomNumber.padStart(6,0);
+    return `#${randomColor.toUpperCase()}`;
+}
+
 // Linking .container from the HTML file
 const container = document.querySelector('.container');
 
@@ -7,4 +17,10 @@ for (let i = 0; i < 256; i++) { // The 256 comes from 16*16
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     container.appendChild(squareDiv);
+
+    // Adding mouseover event listener to each square (hover effect)
+    squareDiv.addEventListener('mouseover', function() {
+        this.style.backgroundColor = generateRandomColor();
+    })
 }
+
