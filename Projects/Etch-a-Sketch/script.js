82bde4609ea63904e042967b2d7aaa407829f64c
BelowNeutral <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-24 15:49:44 -0500

    completed project

--- Projects/Etch-a-Sketch/script.js
+++ Projects/Etch-a-Sketch/script.js
@@ -1,26 +1,61 @@
-// Function that generates a random color
+// Function to generate a random color
 function generateRandomColor() {
     let rgbValue = 0xFFFFFF;
     let randomNumber = Math.random() * rgbValue;
     randomNumber = Math.floor(randomNumber);
     randomNumber = randomNumber.toString(16);
-    let randomColor = randomNumber.padStart(6,0);
+    let randomColor = randomNumber.padStart(6, '0');
     return `#${randomColor.toUpperCase()}`;
+}
+
+// Function to return black color
+function generateBlackInk() {
+    return '#000000';
 }
 
 // Linking .container from the HTML file
 const container = document.querySelector('.container');
 
+// Flags to determine the current drawing mode
+let isBlackInk = false;
+let isRandomInk = false;
+let isEraser = false;
+
+// Adding event listeners to buttons to update the drawing mode
+document.querySelector('.draw-with-black').addEventListener('click', () => {
+    isBlackInk = true;
+    isRandomInk = false;
+    isEraser = false;
+});
+
+document.querySelector('.draw-with-rainbow').addEventListener('click', () => {
+    isBlackInk = false;
+    isRandomInk = true;
+    isEraser = false;
+});
+
+document.querySelector('.eraser').addEventListener('click', () => {
+    isBlackInk = false;
+    isRandomInk = false;
+    isEraser = true;
+});
+
 // ------ Creating the 16x16 square divs ------
-// For-loop: creates grid
-for (let i = 0; i < 256; i++) { // The 256 comes from 16*16
+for (let i = 0; i < 256; i++) {
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     container.appendChild(squareDiv);
 
-    // Adding mouseover event listener to each square (hover effect)
+    // Adding mouseover event listener to each square
     squareDiv.addEventListener('mouseover', function() {
-        this.style.backgroundColor = generateRandomColor();
-    })
+        if (isBlackInk) {
+            this.style.backgroundColor = generateBlackInk();
+        } else if (isRandomInk) {
+            this.style.backgroundColor = generateRandomColor();
+        } else if (isEraser) {
+            this.style.backgroundColor = '';
+        }
+    });
 }
+
 
