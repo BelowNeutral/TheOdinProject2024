5bf466b55e8c4257101194c3afdc626f77e3c11d
Sentriarc <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-27 18:58:32 -0500

    stuck

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,28 +1,31 @@
 // --------- Linking HTML Buttons ---------
 // Input Bar (Screen)
-const SCREEN = document.getElementById('screen');
+const SCREEN = document.querySelector('#screen');
 
 // Clear button
-const CLEAR = document.getElementById('clearBtn');
+const CLEAR = document.querySelector('#clearBtn');
 
 // Operation buttons
-const ADD = document.getElementById('addBtn');
-const SUBTRACT = document.getElementById('subtractBtn');
-const MULTIPLY = document.getElementById('multiplyBtn');
-const DIVIDE = document.getElementById('divideBtn');
-const EQUAL = document.getElementById('equalBtn');
+let num1;
+let num2;
+const ADD = document.querySelector('#addBtn');
+const SUBTRACT = document.querySelector('#subtractBtn');
+const MULTIPLY = document.querySelector('#multiplyBtn');
+const DIVIDE = document.querySelector('#divideBtn');
+const EQUAL = document.querySelector('#equalBtn');
 
 // Number buttons
-const ONE = document.getElementById('oneBtn');
-const TWO = document.getElementById('twoBtn');
-const THREE = document.getElementById('threeBtn');
-const FOUR = document.getElementById('fourBtn');
-const FIVE = document.getElementById('fiveBtn');
-const SIX = document.getElementById('sixBtn');
-const SEVEN = document.getElementById('sevenBtn');
-const EIGHT = document.getElementById('eightBtn');
-const NINE = document.getElementById('nineBtn');
-const ZERO = document.getElementById('zeroBtn');
+const ONE = document.querySelector('#oneBtn');
+const TWO = document.querySelector('#twoBtn');
+const THREE = document.querySelector('#threeBtn');
+const FOUR = document.querySelector('#fourBtn');
+const FIVE = document.querySelector('#fiveBtn');
+const SIX = document.querySelector('#sixBtn');
+const SEVEN = document.querySelector('#sevenBtn');
+const EIGHT = document.querySelector('#eightBtn');
+const NINE = document.querySelector('#nineBtn');
+const ZERO = document.querySelector('#zeroBtn');
+
 
 // --------- Functions For Basic Operations ---------
 // Basic functions (add, sub, mult, div)
@@ -58,37 +61,54 @@ function divide(num1, num2) {
     return expression;
 }
 
-
 // Function that determines which of the above functions to use
-// function operate() {
-//     // If user clicks on add button, call add() function
-//     // if (this.ADD === true) {
-//     //     return add();
-//     // }
-
-//     // If user clicks on subtract button, call subtract() function
-
-//     // If user clicks on multiply button, call multiply() function
-
-//     // If user clicks on divide button, call divide() function
-// }
-
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "x":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            return "invalid";
+    }
+}
 
 // Function that clears everything from the screen
 function clear() {
-    SCREEN.textContent = "";
+    if (CLEAR === true) {
+        document.querySelector('#screen').textContent = "";
+    }
 }
 
 
 // --------- Event Listeners ---------
-// CLEAR.addEventListener('DOMContentLoaded', clear);
+document.addEventListener('DOMContentLoaded', function() {
+    function clearScreen() {
+        SCREEN.textContent = "0"; // Resetting screen to default value
+    }
+
+    CLEAR.addEventListener('click', clearScreen);
+});
+
+
+
+
+
+
+
+
+
 
 
 
 // ---------- Testing ----------
-if (document.getElementById("screen")) {
-    alert("Element exists");
-}
-else {
-    alert("Element does not exist");
-}
+// if (document.getElementById("screen")) {
+//     alert("Element exists");
+// }
+// else {
+//     alert("Element does not exist");
+// }
