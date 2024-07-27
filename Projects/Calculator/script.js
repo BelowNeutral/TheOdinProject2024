5018da083c4a39576f10fedd28b0870af981b868
Sentriarc <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-26 22:34:17 -0500

    completed html & css calculator

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -33,7 +33,7 @@ function divide(num1, num2) {
 
 
 // Function that determines which of the above functions to use
-function operate() {
+// function operate() {
     // If user clicks on add button, call add() function
 
     // If user clicks on subtract button, call subtract() function
@@ -41,5 +41,5 @@ function operate() {
     // If user clicks on multiply button, call multiply() function
 
     // If user clicks on divide button, call divide() function
-}
+// }
 
