a15f105b00d065b2f4244b33a34cecd3eaba9d4e
Sentriarc <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-26 22:07:57 -0500

    completed basic functions & psuedo code for js

---
+++ Projects/Calculator/script.js
@@ -0,0 +1,45 @@
+// Basic functions (add, sub, mult, div)
+function add(num1, num2) {
+    let expression = `${num1} + ${num2}`;
+    let result = num1 + num2;
+    let equal = `= ${result}`;
+    // console.log(result);
+    return expression;
+}
+
+function subtract(num1, num2) {
+    let expression = `${num1} - ${num2}`;
+    let result = num1 - num2;
+    let equal = `= ${result}`;
+    // console.log(result);
+    return expression;
+}
+
+function multiply(num1, num2) {
+    let expression = `${num1} * ${num2}`;
+    let result = num1 * num2;
+    let equal = `= ${result}`;
+    // console.log(result);
+    return expression;
+}
+
+function divide(num1, num2) {
+    let expression = `${num1} / ${num2}`;
+    let result = num1 / num2;
+    let equal = `= ${result}`;
+    // console.log(result);
+    return expression;
+}
+
+
+// Function that determines which of the above functions to use
+function operate() {
+    // If user clicks on add button, call add() function
+
+    // If user clicks on subtract button, call subtract() function
+
+    // If user clicks on multiply button, call multiply() function
+
+    // If user clicks on divide button, call divide() function
+}
+
