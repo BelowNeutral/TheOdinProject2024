3ba19227d064ff1f64a976616959551a492bd465
Sentriarc <tran.christopherd1@gmail.comgit config --global user.anemgit config --global user.email tran.christopherd1@gmail.com>
2024-07-28 23:13:59 -0500

    added functional numpad buttons

--- Projects/Calculator/script.js
+++ Projects/Calculator/script.js
@@ -1,13 +1,17 @@
+// vvvvvvv Declaring Variables vvvvvvv
+let displayValue = 0;
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
 // --------- Linking HTML Buttons ---------
 // Input Bar (Screen)
 const SCREEN = document.querySelector('#screen');
 
 // Clear button
-const CLEAR = document.querySelector('#clearBtn');
+const CLEAR_SCREEN = document.querySelector('#clearBtn');
 
 // Operation buttons
-let num1;
-let num2;
 const ADD = document.querySelector('#addBtn');
 const SUBTRACT = document.querySelector('#subtractBtn');
 const MULTIPLY = document.querySelector('#multiplyBtn');
@@ -15,16 +19,27 @@ const DIVIDE = document.querySelector('#divideBtn');
 const EQUAL = document.querySelector('#equalBtn');
 
 // Number buttons
-const ONE = document.querySelector('#oneBtn');
-const TWO = document.querySelector('#twoBtn');
-const THREE = document.querySelector('#threeBtn');
-const FOUR = document.querySelector('#fourBtn');
-const FIVE = document.querySelector('#fiveBtn');
-const SIX = document.querySelector('#sixBtn');
-const SEVEN = document.querySelector('#sevenBtn');
-const EIGHT = document.querySelector('#eightBtn');
-const NINE = document.querySelector('#nineBtn');
-const ZERO = document.querySelector('#zeroBtn');
+// const ONE = document.querySelector('#oneBtn');
+// const TWO = document.querySelector('#twoBtn');
+// const THREE = document.querySelector('#threeBtn');
+// const FOUR = document.querySelector('#fourBtn');
+// const FIVE = document.querySelector('#fiveBtn');
+// const SIX = document.querySelector('#sixBtn');
+// const SEVEN = document.querySelector('#sevenBtn');
+// const EIGHT = document.querySelector('#eightBtn');
+// const NINE = document.querySelector('#nineBtn');
+// const ZERO = document.querySelector('#zeroBtn');
+
+
+// --------- Function For Displaying Numbers On Screen ---------
+const numPad = document.querySelectorAll('.number');
+for (let i = 0; i < numPad.length; i++) {
+    numPad[i].addEventListener('click', function(event) {
+        SCREEN.value += event.target.textContent;
+    })
+}
+
+
 
 
 // --------- Functions For Basic Operations ---------
@@ -61,41 +76,43 @@ function divide(num1, num2) {
     return expression;
 }
 
-// Function that determines which of the above functions to use
-function operate(operator, num1, num2) {
-    switch (operator) {
-        case "+":
-            return num1 + num2;
-        case "-":
-            return num1 - num2;
-        case "x":
-            return num1 * num2;
-        case "/":
-            return num1 / num2;
-        default:
-            return "invalid";
-    }
-}
 
 // Function that clears everything from the screen
-function clear() {
-    if (CLEAR === true) {
-        document.querySelector('#screen').textContent = "";
-    }
-}
+// function clear() {
+//     if (CLEAR === true) {
+//         document.querySelector('#screen').textContent = "";
+//     }
+// }
+
+
+
+// Function that determines which of the above functions to use
+// function operate(operator, num1, num2) {
+//     switch (operator) {
+//         case "+":
+//             return num1 + num2;
+//         case "-":
+//             return num1 - num2;
+//         case "x":
+//             return num1 * num2;
+//         case "/":
+//             return num1 / num2;
+//         case "C":
+//             return clearScren();
+//         default:
+//             return "invalid";
+//     }
+// }
 
 
 // --------- Event Listeners ---------
-document.addEventListener('DOMContentLoaded', function() {
-    function clearScreen() {
-        SCREEN.textContent = "0"; // Resetting screen to default value
-    }
+// document.addEventListener('DOMContentLoaded', function() {
+//     function clearScreen() {
+//         SCREEN.textContent = 0; // Resetting screen to default value
+//     }
 
-    CLEAR.addEventListener('click', clearScreen);
-});
-
-
-
+//     CLEAR_SCREEN.addEventListener('click', clearScreen);
+// });
 
 
 
@@ -106,9 +123,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 // ---------- Testing ----------
-// if (document.getElementById("screen")) {
+// if (document.querySelector("#screen")) {
 //     alert("Element exists");
 // }
 // else {
 //     alert("Element does not exist");
 // }
+
+// TWO.addEventListener('click', function() {
+//     SCREEN.textContent = 2;
+//     console.log(2);
+// });
